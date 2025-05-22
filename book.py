2c70a5482803f79dabb78f108c43c89eee334402
al-amin040 <alaminium.10@gmail.com>
2025-05-22 21:14:10 +0100

    book assignment

---
+++ book.py
@@ -0,0 +1,16 @@
+name = input("What is your name? ")
+book_title = input("What is the title of our book? ")
+author = input("Who is the author? ")
+number_of_pages = input("How many pages does it include? ")
+fav_quote = input("What's your favourite quote?")
+summary = input("Write a one line summary.")
+
+
+print("-----------------------------------")
+
+print("\\\\\n Book Buddy Report")
+print(name.upper())
+print(book_title[ :20])
+
+
+
---
+++ wordwizard.py
@@ -0,0 +1,11 @@
+sentence = input("Write a sentence you wrote for a school assignment. ")
+old_word = input("Write the word you want to replace. ")
+new_word = input("Write the word you want to replace it with. ")
+
+new_sentence_1 = sentence.strip()
+print(new_sentence_1)
+new_sentence_2 = (sentence.split(","))
+print(new_sentence_2)
+new_sentence_3 = sentence.title()
+print(new_sentence_3)
+
